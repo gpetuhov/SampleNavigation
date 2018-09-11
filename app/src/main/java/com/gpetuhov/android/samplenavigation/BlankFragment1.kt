e7417788bfac587c456b8f3471cf2f8546eace46
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-09-11 18:25:19 +0300

    Navigate between fragments

--- app/src/main/java/com/gpetuhov/android/samplenavigation/BlankFragment1.kt
+++ app/src/main/java/com/gpetuhov/android/samplenavigation/BlankFragment1.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_blank_fragment1.*
 
 // In this sample project we will navigate from BlankFragment1 to BlankFragment2 and back
 
@@ -15,5 +17,15 @@ class BlankFragment1 : Fragment() {
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank_fragment1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Here we navigate from one fragment to another.
+        // Note that we pass id of the action.
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_blankFragment1_to_blankFragment2)
+        }
     }
 }
